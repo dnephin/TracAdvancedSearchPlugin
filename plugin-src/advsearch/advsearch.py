058ae53f59dd0e8f6c16b7c47e009c3cb789dd8d
Daniel Nephin <dnephin@yelp.com>
2011-04-29 23:42:15 -0700

    Added part of the Extension point .

--- plugin-src/advsearch/advsearch.py
+++ plugin-src/advsearch/advsearch.py
@@ -1,5 +1,9 @@
 """
- advsearch.py - Advanced Search Plugin for Trac
+advsearch.py - Advanced Search Plugin for Trac
+
+This module defines a Trac extension point for the advanced search backend.
+
+See TracAdvancedSearchBackend for more details.
 """
 
 import pkg_resources
@@ -12,11 +16,55 @@ from trac.web.main import IRequestHandler
 from trac.wiki.api import IWikiSyntaxProvider
 
 from trac.core import Component
+from trac.core import ExtensionPoint
 from trac.core import implements
 from trac.util import escape
 from trac.util.html import html
 from trac.util.translation import _
 from trac.web.chrome import add_link, add_stylesheet, add_warning
+
+
+class IAdvSearchBackend(Interface):
+	"""Interface to provides a search service."""
+
+	def upsert_document(doc):
+		"""
+		Accepts a dictionary doc which contains all the data about an updated
+		document (wiki page, ticket, etc) to be inserted or updated in the 
+		backend index. The keys of the dict should match the field names in
+		the database.
+		"""
+
+
+	def query_backend(criteria):
+		"""
+		Given a dictionary of criteria, perform a query in the search backend
+		and return a list of dicts with the results. Backends should ignore any
+		criteria it does not know how to deal with.
+
+		Returns a dict with keys: name, score, source, text.  When multiple 
+		providers return results for a source score is used to order the results.
+
+		Example:
+		criteria = {
+			'q': 'trac help',
+			'author: ['admin', 'joe'],
+			'source': ['wiki'],
+			'date_start': '2011-04-01',
+			'date_end': '2011-04-30',
+		}
+
+		return [
+			{
+				'name': 'TracHelp', 
+				'score': 0.876, 
+				'source': 'wiki', 
+				'text': '==Trac Help== ....' 
+			},
+			...
+		]
+		"""
+
 
 class AdvancedSearchPlugin(Component):
 	implements(
@@ -27,6 +75,8 @@ class AdvancedSearchPlugin(Component):
 		ITemplateProvider,
 	)
 	
+	providers = ExtensionPoint(IAdvSearchBackend)
+
 	SOURCE_FILTERS = ('wiki', 'ticket')
 
 	# INavigationContributor methods
@@ -49,8 +99,6 @@ class AdvancedSearchPlugin(Component):
 		# TODO: add /search if search module is disabled
 		return re.match(r'/advsearch?', req.path_info) is not None
 
-
-
 	def process_request(self, req):
 		"""
 		Implements IRequestHandler.process_request
@@ -62,20 +110,58 @@ class AdvancedSearchPlugin(Component):
 
 		query = req.args.get('q')
 		data = {
-			'source_filters': self._get_filter_dicts(self.SOURCE_FILTERS, req.args),
-			'author_filters': req.args.getlist('author_filters'),
-			'date_range_start': req.args.getfirst('date_range_start'),
-			'date_range_end': req.args.getfirst('date_range_end'),
-			'query': query, 
-			'quickjump': None, 
-			'results': []
+			'source': req.args.getlist('source_filters'),
+			'author': req.args.getlist('author_filters'),
+			'date_start': req.args.getfirst('date_range_start'),
+			'date_end': req.args.getfirst('date_range_end'),
+			'q': query, 
 		}
+
+		# perform query using backend
+		result_map = {}
+		for provider in self.providers:
+			result_map[provider.__class__.__name__] = provider.query_backend(data)
+
+		data['source_filters'] = self._get_filter_dicts(self.SOURCE_FILTERS, req.args)
+		data['quickjump'] = None
+		data['results'], data['start_points'] = self._merge_results(result_map)
 
 		add_stylesheet(req, 'common/css/search.css')
 		return 'advsearch.html', data, None
 
-	@classmethod
-	def _get_filter_dicts(cls, filter_list, req_args):
+	def _merge_results(self, result_map):
+		"""
+		Merge results from multiple sources by score in each result. Return
+		a tuple of (results, start_points).  Results is the list of search
+		results to display to the user, and start points are the offsets
+		to use on the next page of results.  These are only needed when
+		multiple providers return results.
+		
+		Example:
+		(
+			// Results
+			[
+				{
+					'title': 'Trac Help', 
+					'href': 'http://...', 
+					'date': '2011-04-20 12:34:00', 
+					'author': 'admin',
+					'summary': '...'
+				},
+				...
+			],
+			// Start points
+			{
+				'PySolrBackend': 8,
+				'SphinxBackend': 4,
+			}
+		)
+		"""
+
+
+
+	def _get_filter_dicts(self, filter_list, req_args):
+		"""Map filters to filter dicts for the frontend."""
 		return [
 			{'name': filter, 'active': req_args.get(filter)}
 			for filter in filter_list
